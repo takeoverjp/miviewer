b96658a0877195c3bd52aebc1332d84262d6400e
Kondo Takeo <takeoverjp@gmail.com>
2019-09-28 08:32:31 +0900

    Draw graph periodically.

--- miviewer.py
+++ miviewer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import numpy as np
 from functools import reduce
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
 
 def get_meminfo():
     with open("/proc/meminfo", "r") as file:
@@ -20,9 +24,40 @@ def parse_meminfo(mi_str):
     mi = reduce(parse_meminfo_line, mi_lines, {})
     return mi
 
-def main():
+def update_graph(frame, x, y, keys, axes):
+    plt.cla()
+
     mi = parse_meminfo(get_meminfo())
-    print(mi)
+
+    x.append(frame)
+    for idx, key in enumerate(keys):
+        y[idx].append(mi[key])
+
+    axes.stackplot(x, np.vstack(y), labels=keys)
+    axes.legend(loc="upper left")
+
+    plt.plot()
+
+def draw_graph(interval, frames, keys):
+    fig, axes = plt.subplots()
+
+    x = []
+    y = [[] for i in keys]
+
+    params = {
+        "fig": fig,
+        "func": update_graph,
+        "fargs": (x, y, keys, axes),
+        "interval": interval * 1000,
+        "frames": frames,
+        "repeat": False,
+    }
+    anime = animation.FuncAnimation(**params)
+
+    plt.show()
+
+def main():
+    draw_graph(1, 10, ["MemFree", "Buffers", "Cached"])
 
 if __name__ == "__main__":
     main()
