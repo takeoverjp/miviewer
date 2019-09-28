1277d1493ae92acadc317e8484d151252a7c7b95
Kondo Takeo <takeoverjp@gmail.com>
2019-09-28 21:28:04 +0900

    Add check function for some meminfo formula.

--- miviewer.py
+++ miviewer.py
@@ -20,6 +20,56 @@ def parse_meminfo_line(mi, line):
     mi[key] = size
     return mi
 
+def check_mem_available(mi):
+    avail1 = mi["MemAvailable"]
+    avail2 = mi["MemFree"] + mi["Inactive(file)"] + mi["SReclaimable"]
+    diff = avail1 - avail2
+    print("MemAvailable diff: {} kB".format(diff))
+
+def check_file_backed(mi):
+    file_backed1 = mi["Buffers"] + mi["Cached"]
+    file_backed2 = mi["Active(file)"] + mi["Inactive(file)"] + mi["Shmem"]
+    diff = file_backed1 - file_backed2
+    print("File Backed diff: {} kB".format(diff))
+
+def check_anon(mi):
+    anon1 = mi["Active(anon)"] + mi["Inactive(anon)"]
+    anon2 = mi["Shmem"] + mi["AnonPages"]
+    diff = anon1 - anon2
+    print("Anonymous diff: {} kB".format(diff))
+
+def check_user_space(mi):
+    user1 = mi["AnonPages"] + mi["Buffers"] + mi["Cached"]
+    user2 = mi["Active(file)"] + mi["Inactive(file)"] + mi["Unevictable"] \
+            + mi["Active(anon)"] + mi["Inactive(anon)"]
+    diff = user1 - user2
+    print("User Space diff: {} kB".format(diff))
+
+def check_slab(mi):
+    slab1 = mi["Slab"]
+    slab2 = mi["SReclaimable"] + mi["SUnreclaim"]
+    diff = slab1 - slab2
+    print("Slab diff: {} kB".format(diff))
+
+def check_total(mi):
+    total1 = mi["MemTotal"]
+    total2 = mi["MemFree"] + mi["Active(file)"] \
+             + mi["Inactive(file)"] + mi["Unevictable"] \
+             + mi["Active(anon)"] + mi["Inactive(anon)"] \
+             + mi["SReclaimable"] + mi["SUnreclaim"] \
+             + mi["KernelStack"] + mi["PageTables"] \
+             + mi["VmallocUsed"]
+    diff = total1 - total2
+    print("Total size diff: {} kB".format(diff))
+
+def check_meminfo(mi):
+    check_mem_available(mi)
+    check_file_backed(mi)
+    check_anon(mi)
+    check_user_space(mi)
+    check_slab(mi)
+    check_total(mi)
+
 def parse_meminfo(mi_str):
     mi_lines = mi_str.splitlines()
     mi = reduce(parse_meminfo_line, mi_lines, {})
@@ -70,6 +120,8 @@ def parse_option():
     ap.add_argument('-c', '--count', type=int,
                            default=None,
                            help='Number of updates.  Default is infinite.')
+    ap.add_argument("--check", action="store_true",
+                           help="Check some formulas.")
     ap.add_argument('-w', '--window', type=int,
                            default=120,
                            help='Number of shown.  Default is 120.')
@@ -77,6 +129,12 @@ def parse_option():
 
 def main():
     args = parse_option()
+
+    if args.check:
+        mi = parse_meminfo(get_meminfo())
+        check_meminfo(mi)
+        exit()
+
     draw_graph(args.interval,
                args.count,
                args.window,
@@ -88,7 +146,6 @@ def main():
                 "Inactive(anon)",
                 "SReclaimable",
                 "SUnreclaim",
-                "Slab",
                 "KernelStack",
                 "PageTables",
                 "VmallocUsed"])
