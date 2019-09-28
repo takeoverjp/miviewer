23fa7f8ea8ed7c5e435e16a46fcf4cd2d16b162b
Kondo Takeo <takeoverjp@gmail.com>
2019-09-28 09:01:51 +0900

    Support to specify interval, count, and window size via arguments.

--- miviewer.py
+++ miviewer.py
@@ -5,6 +5,7 @@ import numpy as np
 from functools import reduce
 import matplotlib.pyplot as plt
 from matplotlib import animation
+from argparse import ArgumentParser
 
 
 def get_meminfo():
@@ -24,7 +25,7 @@ def parse_meminfo(mi_str):
     mi = reduce(parse_meminfo_line, mi_lines, {})
     return mi
 
-def update_graph(frame, x, y, keys, axes):
+def update_graph(frame, axes, x, y, keys, window):
     plt.cla()
 
     mi = parse_meminfo(get_meminfo())
@@ -33,12 +34,17 @@ def update_graph(frame, x, y, keys, axes):
     for idx, key in enumerate(keys):
         y[idx].append(mi[key])
 
+    if len(x) > window:
+        x.pop(0)
+        for yelem in y:
+            yelem.pop(0)
+
     axes.stackplot(x, np.vstack(y), labels=keys)
     axes.legend(loc="upper left")
 
     plt.plot()
 
-def draw_graph(interval, frames, keys):
+def draw_graph(interval, frames, window, keys):
     fig, axes = plt.subplots()
 
     x = []
@@ -47,7 +53,7 @@ def draw_graph(interval, frames, keys):
     params = {
         "fig": fig,
         "func": update_graph,
-        "fargs": (x, y, keys, axes),
+        "fargs": (axes, x, y, keys, window),
         "interval": interval * 1000,
         "frames": frames,
         "repeat": False,
@@ -56,8 +62,25 @@ def draw_graph(interval, frames, keys):
 
     plt.show()
 
+def parse_option():
+    ap = ArgumentParser()
+    ap.add_argument('-i', '--interval', type=int,
+                           default=1,
+                           help='The delay between updates in seconds. Default is 1.')
+    ap.add_argument('-c', '--count', type=int,
+                           default=None,
+                           help='Number of updates.  Default is infinite.')
+    ap.add_argument('-w', '--window', type=int,
+                           default=120,
+                           help='Number of shown.  Default is 120.')
+    return ap.parse_args()
+
 def main():
-    draw_graph(1, 10, ["MemFree", "Buffers", "Cached"])
+    args = parse_option()
+    draw_graph(args.interval,
+               args.count,
+               args.window,
+               ["MemFree", "Buffers", "Cached"])
 
 if __name__ == "__main__":
     main()
